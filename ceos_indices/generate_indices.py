94315e7d0109715a624c1fae37642c62644f7fba
Your Name <you@example.com>
2022-07-31 11:24:27 -0600

    skeleton trunk file and logging

--- ceos_indices/generate_indices.py
+++ ceos_indices/generate_indices.py
@@ -1,5 +1,19 @@
+import logging, coloredlogs
 from typing import Dict
+
+logger = logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG', logger=logger)
 
 
 def indices(params: Dict[str, str]):
-    pass
+    logger.info("Loading Input Data")
+
+    logger.info("Preprocessing Images")
+
+    logger.info("Splitting Validation Data")
+
+    logger.info("Calculating Indices")
+
+    logger.info("Validating Indices")
+
+    logger.info("Writing Results")
