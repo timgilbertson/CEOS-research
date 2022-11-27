45d3e980e5b67c092bfdfae7baa5275d4fe6fa5e
TIM <tdgilbertson@gmail.com>
2022-11-27 00:33:33 +0000

    preserve date index on outputs

--- ceos_indices/io/outbound.py
+++ ceos_indices/io/outbound.py
@@ -8,4 +8,4 @@ def write_outputs(outputs: pd.DataFrame, output_path: str):
         outputs (pd.DataFrame): Calculated sensor location indices
         output_path (str): Path to write data
     """
-    outputs.to_csv(output_path + "sensor_indices.csv", index=False)
+    outputs.to_csv(output_path + "sensor_indices.csv", index=True)
